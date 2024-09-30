ba850b6779661d1121e95df07af2669abb1e05bd
Schmiddy2000 <luca.schmitt@gmx.net>
2024-09-30 20:42:12 +0200

    updated reader function and file structure

--- test/read_csv.py
+++ test/read_csv.py
@@ -8,26 +8,6 @@ from matplotlib import pyplot as plt
 from fit_peaks import fit_skew_gaussian
 
 
-def get_data(file_index: int, use_average: bool = False) -> Tuple[np.array, np.array, np.array]:
-    file_path = f'../data/oscilloscope_data/NewFile{file_index}.csv'
-    df = pd.read_csv(file_path)
-
-    channel_df = df.drop([0], axis=0)
-    channel_df = channel_df[['X', 'CH1', 'CH2']]
-    channel_df.columns = ['x', 'ch_one', 'ch_two']
-    channel_df = channel_df.astype(np.float64)
-
-    x = channel_df.x.to_numpy()
-    ch_1 = channel_df.ch_one.to_numpy()
-    ch_2 = channel_df.ch_two.to_numpy()
-
-    if use_average:
-        ch_1 = averager(ch_1)
-        ch_2 = averager(ch_2)
-
-    return x, ch_1, ch_2
-
-
 # fit_skew_gaussian(averager(channel_df.ch_one.to_numpy()), 47, 56, True, True)
 
 # plt.figure(figsize=(12, 5))
--- tools.py
+++ tools.py
@@ -1,5 +1,28 @@
 import numpy as np
 from copy import copy
+import pandas as pd
+
+from typing import Tuple
+
+
+def get_csv_data(file_index: int, use_average: bool = False) -> Tuple[np.array, np.array, np.array]:
+    file_path = f'../data/oscilloscope_data/NewFile{file_index}.csv'
+    df = pd.read_csv(file_path)
+
+    channel_df = df.drop([0], axis=0)
+    channel_df = channel_df[['X', 'CH1', 'CH2']]
+    channel_df.columns = ['x', 'ch_one', 'ch_two']
+    channel_df = channel_df.astype(np.float64)
+
+    x = channel_df.x.to_numpy()
+    ch_1 = channel_df.ch_one.to_numpy()
+    ch_2 = channel_df.ch_two.to_numpy()
+
+    if use_average:
+        ch_1 = averager(ch_1)
+        ch_2 = averager(ch_2)
+
+    return x, ch_1, ch_2
 
 
 def averager(counts_array: np.array):
